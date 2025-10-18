e8ac050380f343ac8c483d9fd9b643ffda34e3d2
iesora <sorasorasora0718@gmail.com>
2025-10-18 16:20:01 +0900

    tiny fix

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,9 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['https://koudaunsou.jp/'],
-    methods: ['POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: false,
+    credentials: true,
   });
   await app.listen(process.env.PORT);
 }
