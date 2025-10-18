f51a968071a84ecc831db22dd06e9333d82eff90
iesora <sorasorasora0718@gmail.com>
2025-10-18 15:47:57 +0900

    tiny fix

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,11 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [/^https?:\/\/(www\.)?your-wordpress-domain\.com$/],
+    origin: [
+      /^https?:\/\/(www\.)?your-wordpress-domain\.com$/,
+      'https://koudaunsou.jp/',
+    ],
     methods: ['POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: false,
   });
-  await app.listen(process.env.PORT);
+  await app.listen(9000);
 }
 bootstrap();
